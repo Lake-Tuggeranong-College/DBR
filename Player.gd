8fa4967502ede9e1d8f88a7d8aed278788311796
Glovania <9937412@schoolsnet.act.edu.au>
2024-08-12 21:49:20 +1000

    Disable TPP feature for now
    Too many bugs occur with this feature when on multiplayer. I will turn it off by now. Put it back on when it is fully tested.

--- Player.gd
+++ Player.gd
@@ -14,17 +14,8 @@ enum DynamicCameraViewToggleAction {
 @onready var fpp_muzzle_flash: GPUParticles3D = $FPPCamera/FPPPistol/MuzzleFlash
 @onready var fpp_raycast: RayCast3D = $FPPCamera/FPPRayCast3D
 
-# Third Player View (TPP)
-@onready var tpp_camera: Camera3D = $TPPCamera
-@onready var tpp_pistol: Node3D = $TPPCamera/TPPPistol
-@onready var tpp_muzzle_flash: GPUParticles3D = $TPPCamera/TPPPistol/MuzzleFlash
-@onready var tpp_raycast: RayCast3D = $TPPCamera/TPPRayCast3D
-
 # Animations
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
-
-# Set player's current camera view state in the editor
-@export var camera_player_state: DynamicCameraViewToggleAction = DynamicCameraViewToggleAction.FIRST_PERSON_VIEW
 
 var health = 3
 var MAX_HEALTH = 10
@@ -35,9 +26,6 @@ const JUMP_VELOCITY = 10.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 20.0
-
-# Track different state of camera node to toggle either FPP or TPP.
-var is_fpp: bool = true
 
 
 func _enter_tree():
@@ -53,56 +41,22 @@ func _ready():
 	if not is_multiplayer_authority(): return
 	
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
-	# Check if player was set FPP as default in editor
-	if camera_player_state == DynamicCameraViewToggleAction.FIRST_PERSON_VIEW:
-		# Indicate that FPP mode is enable
-		is_fpp = true
-		
-		# Ensure FPP camera is active as intially when first load the player
-		fpp_camera.current = true
-		tpp_camera.current = false
-	
-		# Ensure FPP guns are the only one can be see when first load the player
-		fpp_pistol.visible = true
-		tpp_pistol.visible = false
-
-	# If not, then check if player was set TPP as default in editor
-	elif camera_player_state == DynamicCameraViewToggleAction.THIRD_PERSON_VIEW:
-		# Indicate that FPP mode is not enable
-		is_fpp = false
-
-		# Ensure TPP camera is active as intially when first load the player
-		fpp_camera.current = false
-		tpp_camera.current = true
-	
-		# Ensure TPP guns are the only one can be see when first load the player
-		fpp_pistol.visible = false
-		tpp_pistol.visible = true
+	fpp_camera.current = true
 
 
 func _unhandled_input(event):
 	if not is_multiplayer_authority(): return
 
 	if event is InputEventMouseMotion:
-		if is_fpp:
-			rotate_y(-event.relative.x * .005)
-			fpp_camera.rotate_x(-event.relative.y * .005)
-			fpp_camera.rotation.x = clamp(fpp_camera.rotation.x, -PI/2, PI/2)
-		else:
-			rotate_y(-event.relative.x * .005)
-			tpp_camera.rotate_x(-event.relative.y * .005)
-			tpp_camera.rotation.x = clamp(tpp_camera.rotation.x, -PI/2, PI/2)
+		rotate_y(-event.relative.x * .005)
+		fpp_camera.rotate_x(-event.relative.y * .005)
+		fpp_camera.rotation.x = clamp(fpp_camera.rotation.x, -PI/2, PI/2)
 
 	if Input.is_action_just_pressed("shoot") \
 			and anim_player.current_animation != "shoot":
 		play_shoot_effects.rpc()
 		if fpp_raycast.is_colliding():
 			var hit_player = fpp_raycast.get_collider()
-			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
-
-		elif tpp_raycast.is_colliding():
-			var hit_player = tpp_raycast.get_collider()
 			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 
 
@@ -153,21 +107,13 @@ func _input(event):
 	elif event.is_action_pressed("inventory_slot_2"):
 		Global.switch_gun(2)
 
-# Switch player's camera view according to the key inputs set for it
-	if event.is_action_pressed("dynamic_camera_view"):
-		toggle_different_camera_state();
-
 
 @rpc("call_local")
 func play_shoot_effects():
 	anim_player.stop()
 	anim_player.play("shoot")
-	if is_fpp:
-		fpp_muzzle_flash.restart()
-		fpp_muzzle_flash.emitting = true
-	else:
-		tpp_muzzle_flash.restart()
-		tpp_muzzle_flash.emitting = true
+	fpp_muzzle_flash.restart()
+	fpp_muzzle_flash.emitting = true
 
 
 @rpc("any_peer")
@@ -199,22 +145,3 @@ func add_health(additional_health):
 # Handle weapon switching based on the key inputs
 func _on_weapon_switched(gun_name):
 	print("Switched to gun: %s" % gun_name)
-	
-
-# Handle diffrent state of player's camera view
-func toggle_different_camera_state():
-	is_fpp = not is_fpp
-	update_camera_visibility()
-	update_gun_model_visibility()
-
-
-# Update player's camera view when player pressed the pre-defined key input
-func update_camera_visibility(): 
-	fpp_camera.current = is_fpp
-	tpp_camera.current = not is_fpp
-
-
-# Update the visibility of guns when player changed the camera view based on their preferrance
-func update_gun_model_visibility():
-	fpp_pistol.visible = is_fpp
-	tpp_pistol.visible = not is_fpp
